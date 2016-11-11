e83142398ffba87ca8d4bdf4e9e54b00e08352eb
Ad115 <a.garcia230395@gmail.com>
2016-11-10 18:38:23 -0600

    Minor change

--- add.cu
+++ add.cu
@@ -21,7 +21,7 @@ int main()
 
     printf("2 + 3 = %d \n", c);
 	
-	printf("Errors found: cudaMalloc -> %s, cudaMemcpy -> %s\n", 
+	printf("Errors found: \n\tcudaMalloc -> %s, \n\tcudaMemcpy -> %s\n", 
 		   cudaGetErrorString( errorMalloc ),
 		   cudaGetErrorString( errorMemcpy )
 	);
